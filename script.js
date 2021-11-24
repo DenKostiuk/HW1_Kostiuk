89f6b356a7d94e776955a314ba4080d94eec817a
Denys Kostiuk <93026512+DenKostiuk@users.noreply.github.com>
2021-11-24 14:06:19 +0200

    4

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function calcSalary () {
-    
+let a = +prompt('Количество рабочих часов в день');
+let b = +prompt('Количество рабочих дней в недели');
+let c = +prompt('Рейт за час');
+
+
+function calcSalary (a, b, c) {
+    let x = c * a;
+    let y = x * b;
+    alert('$' + y);
 }
+
+calcSalary (a, b, c);
+
