e3ff0bf63c4878283de9d34bde28dd9f0ad4d25e
Denys Kostiuk <93026512+DenKostiuk@users.noreply.github.com>
2021-11-24 14:50:00 +0200

    6

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-//Цикл FOR
-for (let i = 0; i < 5; i++) {
-    alert(i);
-}
+let name = prompt('Введите Ваше имя?');
+let country = prompt('В какой стране Вы проживаете?');
+let city = prompt('В каком городе Вы проживаете?');
 
-//Цикл WHILE
-let i = 0;
-
-while (i < 5) {
-    i++;
-    alert(i);
-}
-
-//Цикл DO..WHILE
-do {
-    i = i + 1;
-    alert(i);
-} while (i < 5);
+console.log('Hello, your name is ' + name + '. You live in ' + city + ', ' + country);
